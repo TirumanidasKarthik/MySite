509cdd93a95fb7de5659b38ced9b5b3d8e6e5410
TirumanidasKarthik <karthiktirumanidas@gmail.com>
2020-09-09 16:30:11 +0530

    new page

--- README.md
+++
@@ -1,2 +0,0 @@
-# MySite
-Personal Website
--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function res(){
-    window.open("Resume333.docx");
+function projects(){
+  window.open("https://github.com/TirumanidasKarthik?tab=repositories");
 }
-function namef(){
-  var x = document.getElementById("u_name").value;
-  var rem = document.getElementById("u_name");
-  var butt = document.getElementById("butt");
-  rem.remove();
-  butt.remove();
-  document.getElementById("par").innerHTML = "Thankyou for visiting "+ x;
+function about(){
+  let s = document.querySelector("#About");
+  let l = s.style.display;
+  if(l == "" || l == "none"){
+    document.getElementById("About").style.display = "block";
+  }
+  if(l == "block"){
+    document.getElementById("About").style.display = "none";
+  }
 
+}
+function drawing(){
+  window.open("drawings.html", "_self");
+}
+function home(){
+  window.open("index.html", "_self");
 }
