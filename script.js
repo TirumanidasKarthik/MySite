b98fbfd9f1fc9b20a95b6572e06eba9d12a39d5f
TirumanidasKarthik <karthiktirumanidas@gmail.com>
2020-10-21 20:22:45 +0530

    optimised drawingview

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function projects(){
   window.open("https://github.com/TirumanidasKarthik?tab=repositories");
 }
@@ -17,4 +19,52 @@ function drawing(){
 }
 function home(){
   window.open("index.html", "_self");
+}
+var index = 1;
+function increment(){
+  index++;
+  nextpage(index);
+}
+function decrement(){
+  index--;
+  prepage(index);
+}
+function nextpage(currval){
+  if(currval == 2){
+    document.getElementById("prebt").style.display = "";
+    document.getElementById("first").style.display = "none";
+    document.getElementById("second").style.display = "contents";
+
+  }
+  if(currval == 3){
+    document.getElementById("second").style.display = "none";
+    document.getElementById("third").style.display = "contents";
+  }
+  if(currval == 4){
+    document.getElementById("third").style.display = "none";
+    document.getElementById("fourth").style.display = "contents";
+    document.getElementById("nexbt").style.display = "none";
+  }
+
+}
+function prepage(currval){
+  if(currval == 1){
+    document.getElementById("prebt").style.display = "none";
+    document.getElementById("nexbt").style.display = "";
+    document.getElementById("first").style.display = "contents";
+    document.getElementById("second").style.display = "none";
+
+  }
+  if(currval == 2){
+    document.getElementById("third").style.display = "none";
+    document.getElementById("second").style.display = "contents";
+
+  }
+  if(currval == 3){
+    document.getElementById("fourth").style.display = "none";
+    document.getElementById("third").style.display = "contents";
+    document.getElementById("nexbt").style.display = "";
+
+  }
+
 }
